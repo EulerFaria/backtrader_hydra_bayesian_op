5a4b891de08ee956f6145e069194d346f46d3a21
EulerFaria <eulerodriguesousa@gmail.com>
2020-06-27 15:03:23 -0300

    update report.py for creating .csv file in case it doesnt exist

--- report.py
+++ report.py
@@ -1,16 +1,28 @@
-
+import os
 import pandas as pd 
 
+_file_path = os.path.dirname(os.path.abspath(__file__))
 
 def generate_report(dct,cfg):
 
-    tmp=pd.read_csv('D:/Documents/Projetos/bot-dos-brother/results.csv')
+    if os.path.exists(_file_path +"results.csv"):
+        current_csv =pd.read_csv(_file_path +"/results.csv")
     
-    tmp1= pd.DataFrame({"stock":[cfg.stock.name],
-                        "period":[cfg.grain.period],
-                        "strategy":[dct['strategy']],
-                        "params":[str(dct['params'])],
-                        "target":[dct['target']]
-                         })
+        tmp= pd.DataFrame({"stock":[cfg.stock.name],
+                            "period":[cfg.grain.period],
+                            "strategy":[dct['strategy']],
+                            "params":[str(dct['params'])],
+                            "target":[dct['target']]
+                            })
 
-    pd.concat([tmp,tmp1],ignore_index=True).to_csv('D:/Documents/Projetos/bot-dos-brother/results.csv',index=False)
+        pd.concat([current_csv,tmp],ignore_index=True).to_csv(_file_path +"/results.csv",index=False)
+
+    else:
+        tmp= pd.DataFrame({"stock":[cfg.stock.name],
+                    "period":[cfg.grain.period],
+                    "strategy":[dct['strategy']],
+                    "params":[str(dct['params'])],
+                    "target":[dct['target']]
+                    })
+
+        tmp.to_csv(_file_path +"/results.csv",index=False)
